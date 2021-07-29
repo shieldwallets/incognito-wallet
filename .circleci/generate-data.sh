0c027f0131409d616eca97e76ef1be25a8371969
Duy Huynh <duy@incognito.org>
2021-07-30 03:22:02 +0700

    APPSTORE_CERT

--- .circleci/generate-data.sh
+++ .circleci/generate-data.sh
@@ -13,5 +13,5 @@ NODE_USER_NAME=$NODE_USER_NAME
 echo $KEYSTORE | base64 -di | tee android/app/wallet-app-release-key.keystore
 echo $PLAY_STORE_JSON | base64 -di | tee play_store.json
 echo $APPLE_STORE_DEVELOP_KEY | base64 -di | tee fastlane/develop.json
-
-
+echo $APPSTORE_MOBILE_PROVISION | base64 -di | tee fastlane/AppStore_com.incognito.wallet.mobileprovision
+echo $APPSTORE_CERT | base64 -di | tee fastlane/cert.p12
